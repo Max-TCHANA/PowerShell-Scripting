702ce4b8714dc5b1f8ba9d0ddd39b6bfa30638fd
Max Tchana <max.tchana@autowit.co>
2020-06-15 14:59:09 -0600

    Activity 4

---
+++ A4-Difference-Between-Write-Host-And-Write-Output.ps1
@@ -0,0 +1,20 @@
+﻿<# https://www.itprotoday.com/powershell/write-output-or-write-host-powershell
+
+** If you possibly want to write data to the screen, but also want the data to be passed down the pipeline to further commands, then use Write-Output. 
+** If you only want data to display to the screen, use Write-Host. Write-Host does not allow the data to be passed down the pipeline to further commands.
+
+
+#>
+
+function Receive-Output 
+{
+    process { Write-Host $_ -ForegroundColor Green }
+}
+
+Write-Output "this is a test" | Receive-Output
+Write-Host "this is a test" | Receive-Output
+Write-Output "this is a test"
+
+Write-Host "You are looking " -NoNewline
+Write-Host "AWESOME" -ForegroundColor Red -BackgroundColor Yellow -NoNewline
+Write-Host " today"
